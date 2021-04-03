6dd9ddcb23c97a6b8907e96e560c5c444597cae5
Ilya Nevolin <justdoitzlol@gmail.com>
2021-04-03 19:17:46 +0200

    solution 01 - partial

---
+++ solution_01/README.md
@@ -0,0 +1,11 @@
+# Availability Solution 01
+
+## Requirements
+NodeJS v12+ with NPM
+
+## Usage
+1. run `npm install` (once)
+2. run `npm start`
+3. open `http://localhost:8000`
+
+Hint: use `nodemon` to make the app restart automatically on changes.
---
+++ solution_01/server.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.use('/', express.static('./public'))
+
+const router = express.Router({
+    caseSensitive: app.get('case sensitive routing'),
+    strict       : app.get('strict routing')
+});
+app.use(router);
+
+// temporary JSON-file as database
+let STATE;
+if (!fs.existsSync('data.json')) {
+  fs.appendFileSync('data.json', '{}');
+  STATE = {}
+} else {
+  STATE = JSON.parse(fs.readFileSync('data.json'));
+}
+
+function save() {
+  fs.writeFileSync('data.json', JSON.stringify(STATE, null, 2));
+}
+
+// routes
+router.post('/api/meeting/', (req, res) => {
+  let ID = new Date().getTime();
+  STATE[ID] = {
+    dates: req.body.dates,
+    answers: [],
+  }
+  save();
+  res.send({success:1, ID: ID})
+});
+
+router.get('/api/meeting/:ID', (req, res) => {
+  // console.log(req.params.ID)
+  res.send({success:1, dates:STATE[req.params.ID].dates})
+});
+
+router.post('/api/meeting/:ID', (req, res) => {
+  // console.log(req.body)
+  STATE[req.params.ID].answers.push(req.body);
+  save();
+  res.send({success:1})
+});
+
+
+router.get('/api/meeting/results/:ID', (req, res) => {
+  // console.log(req.params.ID)
+  res.send({success:1, data:STATE[req.params.ID]})
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
