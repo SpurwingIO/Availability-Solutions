2eed6a5ca84cdbe287addd4374da207ec17ee5a0
healzer <justdoitzlol@gmail.com>
2021-04-11 10:27:31 +0000

    eco

--- solution_01/ecosystem.config.js
+++ solution_01/ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     autorestart: true,
     log_date_format: 'HH:mm:ss',
     watch: true,
-    ignore_watch : ["db", "tmp", ".git"],
+    ignore_watch : ["db", "tmp", ".git", "node_modules"],
     max_memory_restart: '2G',
   }]
 };
